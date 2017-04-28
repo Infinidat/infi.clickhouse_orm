2ababe8b5e8c00b475b8232a9aadb411be58b7ae
Itai Shirav <itais@infinidat.com>
2017-04-28 19:03:34 +0300

    refactor documentation

--- scripts/docs2html.sh
+++ scripts/docs2html.sh
@@ -3,5 +3,8 @@ mkdir -p ../htmldocs
 
 find ./ -iname "*.md" -type f -exec sh -c 'echo "Converting ${0}"; pandoc "${0}" -s -o "../htmldocs/${0%.md}.html"' {} \;
 
+echo "Converting README"
+pandoc ../README -s -o "../htmldocs/README.html"
+
 echo "Fixing links"
 sed -i 's/\.md/\.html/g' ../htmldocs/*.html
