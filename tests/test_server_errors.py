3bbf06a70ac13cefebdcf743a4bb863e42366d9c
Itai Shirav <itais@infinidat.com>
2019-02-26 23:24:45 +0200

    TRIVIAL assertEquals ==> assertEqual

--- tests/test_server_errors.py
+++ tests/test_server_errors.py
@@ -9,24 +9,24 @@ class ServerErrorTest(unittest.TestCase):
     def test_old_format(self):
 
         code, msg = ServerError.get_error_code_msg("Code: 81, e.displayText() = DB::Exception: Database db_not_here doesn't exist, e.what() = DB::Exception (from [::1]:33458)")
-        self.assertEquals(code, 81)
-        self.assertEquals(msg, "Database db_not_here doesn't exist")
+        self.assertEqual(code, 81)
+        self.assertEqual(msg, "Database db_not_here doesn't exist")
 
         code, msg = ServerError.get_error_code_msg("Code: 161, e.displayText() = DB::Exception: Limit for number of columns to read exceeded. Requested: 11, maximum: 1, e.what() = DB::Exception\n")
-        self.assertEquals(code, 161)
-        self.assertEquals(msg, "Limit for number of columns to read exceeded. Requested: 11, maximum: 1")
+        self.assertEqual(code, 161)
+        self.assertEqual(msg, "Limit for number of columns to read exceeded. Requested: 11, maximum: 1")
 
 
     def test_new_format(self):
 
         code, msg = ServerError.get_error_code_msg("Code: 164, e.displayText() = DB::Exception: Cannot drop table in readonly mode")
-        self.assertEquals(code, 164)
-        self.assertEquals(msg, "Cannot drop table in readonly mode")
+        self.assertEqual(code, 164)
+        self.assertEqual(msg, "Cannot drop table in readonly mode")
 
         code, msg = ServerError.get_error_code_msg("Code: 48, e.displayText() = DB::Exception: Method write is not supported by storage Merge")
-        self.assertEquals(code, 48)
-        self.assertEquals(msg, "Method write is not supported by storage Merge")
+        self.assertEqual(code, 48)
+        self.assertEqual(msg, "Method write is not supported by storage Merge")
 
         code, msg = ServerError.get_error_code_msg("Code: 60, e.displayText() = DB::Exception: Table default.zuzu doesn't exist.\n")
-        self.assertEquals(code, 60)
-        self.assertEquals(msg, "Table default.zuzu doesn't exist.")
+        self.assertEqual(code, 60)
+        self.assertEqual(msg, "Table default.zuzu doesn't exist.")
